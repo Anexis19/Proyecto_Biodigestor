7b08d1c883784724d6e90634fcb607b4260b1652
Andres Hurtado <ahpayp2@gmail.com>
2022-05-22 11:06:11 -0500

    commit rama develop1. 22/05/2022 Se realiza el registro correcto de un usuario en la base de datos. Falta realizar validacion de campos del formulario y crear catalogo de productos. Se crea una carpeta logica en donde se realizaran las peticiones PHP sin afectar las paginas de html. Evitando asi la insercion de PHP innecesario en un documento HTML

---
+++ logic/form_register.php
@@ -0,0 +1,28 @@
+<?php
+    require '../conexion.php';
+    
+    // Se reciben los datos del formulario
+   $nombre_usuario  =   $_POST['nombre_usuario'];
+   $fecha_nac       =   $_POST['fecha_nac'];
+   $tipo_doc        =   $_POST['tipo_doc'];
+   $numero_id       =   $_POST['numero_id'];
+   $direccion       =   $_POST['direccion'];
+   $depart          =   $_POST['depart'];
+   $munic           =   $_POST['munic'];
+   $tel             =   $_POST['tel'];
+   $pasw            =   $_POST['pasw'];
+
+   $registrar = "INSERT INTO users VALUES ('$numero_id','$nombre_usuario', '$fecha_nac', '$tipo_doc', '$direccion', '$depart', '$munic', '$tel', '$pasw')";
+   $query = mysqli_query($conectar, $registrar);
+
+   if($query){
+       echo "<script> alert('Registro existoso'); 
+       location.href = '../index.html';
+       </script>";
+   }
+   else{
+       echo "<script> alert('Registro incorrecto'); 
+       location.href = '../index.html';
+       </script>";
+   }
+?>
